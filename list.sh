c24d178756fa0ade807dde71055ddff1b0f13560
gregoire renard <gregoire.renard@epitech.eu>
2017-08-29 18:12:06 +0200

    projet

---
+++ list.sh
@@ -0,0 +1,11 @@
+#!/bin/sh
+## list.sh for list in /home/renard_e/2/SHL_2016_shell
+## 
+## Made by Gregoire Renard
+## Login   <renard_e@epitech.net>
+## 
+## Started on  Tue Jan  3 20:23:31 2017 Gregoire Renard
+## Last update Tue Jan  3 20:24:50 2017 Gregoire Renard
+##
+
+echo -e "bonjour"
